b066dd370a8e00f731d92f954ea6f4048da91b80
shreenivasssss <sreenivassss@gmail.com>
2018-08-11 00:37:36 +0530

    initial project files

---
+++ mod2/src/main/java/niranta/Account.java
@@ -0,0 +1,15 @@
+package niranta;
+
+public class Account {
+    private String number;
+    private String name;
+
+
+    public Account(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+}
